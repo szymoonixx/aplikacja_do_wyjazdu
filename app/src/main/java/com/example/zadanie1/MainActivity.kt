e168ad5108f8362033de94db57f0c9f09a5ee27a
piasek <piaskowski3456@gmail.com>
2022-12-14 12:37:59 +0100

    123

--- app/src/main/java/com/example/zadanie1/MainActivity.kt
+++ app/src/main/java/com/example/zadanie1/MainActivity.kt
@@ -3,19 +3,32 @@ package com.example.zadanie1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.*
+import kotlin.time.Duration.Companion.days
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var data_w=findViewById<TextView>(R.id.wyjazdD)
-        var data_p=findViewById<TextView>(R.id.powrotD)
+        var wyjazd=0
+        var powrot=2
         var oblicz=findViewById<Button>(R.id.obliczanie)
         var ilosc=findViewById<TextView>(R.id.ilosc)
+        var data_w=findViewById<Button>(R.id.Wyjazd)
+        var data_p=findViewById<Button>(R.id.powrot)
+        var kalendarz=findViewById<CalendarView>(R.id.calendarView)
+
+        data_w.setOnClickListener {
+            wyjazd = kalendarz.date.days.toString().toInt()
+        }
+        data_p.setOnClickListener {
+            powrot = kalendarz.date.days.toString().toInt()
+
+        }
+
         oblicz.setOnClickListener {
-            var i=data_p.text.toString().toInt()-data_w.text.toString().toInt()
-            ilosc.text = "Ilosc dni: " + i.toString()
+            var suma =powrot-wyjazd
+            ilosc.text = "Ilosc dni: "+ suma.toString()
         }
 
     }
